e89968451c8b74da1d5b10c8d9d6d5559b48bc10
Damon S. Green II <damonsgreen2@gmail.com>
2025-05-29 02:54:47 -0400

    movement setup and scene and player prefabs made
    replace strafe anims
    
    change player script

--- IuvoRPG/Assets/IuvoRPG/RPG/TestPlayer.cs
+++ IuvoRPG/Assets/IuvoRPG/RPG/TestPlayer.cs
@@ -1,12 +1,34 @@
 using UnityEngine;
 using IuvoUnity._Input._Controllers;
+using IuvoUnity._BaseClasses._InputBases._Legacy;
+using IuvoUnity._DataStructs;
 
 public class TestPlayer : PlayerControllerBase
 {
     public float speed = 5f;
     public float jumpForce = 5f;
+    public float jumpReEnter = 0.5f;
+    public float currentJumpTime;
     public bool isOnGround = false;
     //public bool isForcingDown = false;
+    [SerializeField] public CapsuleCollider capsuleCollider;
+
+    public float levitateForce = 0.75f;
+    public KeyHoldInputAction levitateInput;
+
+    Animator animator;
+
+    protected override void SetupInputs()
+    {
+        base.SetupInputs();
+        levitateInput = gameObject.AddComponent<KeyHoldInputAction>();
+        levitateInput.SetKey(KeyCode.E);
+        levitateInput.neededHoldTime = 0.20f;
+        levitateInput.holdTimeRange = (new RangeF(levitateInput.neededHoldTime, 1.25f));
+
+        levitateInput.name = "Levitate Action";
+        levitateInput.OnPerformed += OnLevitate;
+    }
 
     protected override void Awake()
     {
@@ -14,6 +36,16 @@ public class TestPlayer : PlayerControllerBase
         SetupInputs();
         IsEnabled = true;
         IsActive = true;
+        animator = GetComponentInParent<Animator>();
+
+        if (capsuleCollider != null)
+        {
+            groundCheck.checkOrigin = capsuleCollider.bounds.min;
+            groundCheck.distanceToCheck = 0.75f;
+            groundCheck.radiusToCheck = capsuleCollider.radius;
+        }
+        levitateForce = jumpForce / 3.0f;
+
     }
 
     protected override void Update()
@@ -26,27 +58,47 @@ public class TestPlayer : PlayerControllerBase
             {
                 isOnGround = IsGrounded();
             }
-
-            //if (!isOnGround && activelyApplyingGravity)
-            //{
-            //    isForcingDown = true;
-            //}
-            //else
-            //{
-            //    isForcingDown = false;
-            //}
         }
     }
 
     protected override void OnJump()
     {
+        if (currentJumpTime == 0 || (currentJumpTime - Time.time) > jumpReEnter)
+        {
+            currentJumpTime = Time.time;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            animator.SetTrigger("StartJump");
+            return;
+        }
+
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        currentJumpTime = 0;
+
+    }
+
+    protected virtual void OnLevitate()
+    {
+        animator.SetTrigger("StartJump");
+
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Acceleration);
+
     }
 
     public bool IsGrounded()
     {
-        return groundCheck.isGrounded;
-        //return Physics.Raycast(transform.position, Vector3.down, 1.1f);
+        if (!groundCheck.isGrounded)
+        {
+            animator.SetBool("IsFalling", true);
+            isOnGround = false;
+            return false;
+        }
+        else
+        {
+            isOnGround = true;
+            animator.SetBool("IsGrounded", true);
+            animator.SetBool("IsFalling", false);
+            return true;
+        }
     }
 
     protected override void HandleMoveInput()
@@ -54,6 +106,8 @@ public class TestPlayer : PlayerControllerBase
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         moveInput = new Vector2(horizontal, vertical).normalized;
+        animator.SetFloat("Direction", horizontal);
+        animator.SetFloat("Velocity", vertical);
 
     }
 }
